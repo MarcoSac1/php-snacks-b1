f1301759474361f1a6b75d112349daeed4a83d56
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-26 12:53:36 +0200

    fixed array name issue

--- index.php
+++ index.php
@@ -3,32 +3,34 @@
         [
             'team_1' => 'Olimpia Milano',
             'point_1' => '55',
-            'name_2' => 'Cantù',
+            'team_2' => 'Cantù',
             'point_2' => '60'
         ],
         [
-            
+            'team_1' => 'Fortitudo Verona',
+            'point_1' => '102',
+            'team_2' => 'Orlandina Basket',
+            'point_2' => '34'
         ],
     ];
     // var_dump($game);
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-snacks-b1</title>
-</head>
-<body>
-    <main>
-        <ul>
-            <?php foreach ($games as $game){ ?>
-            <li> <?php echo $game['team_1']; ?> - <?php echo $game['team_2']; ?> | <?php echo $game['point_1']; ?> <?php echo $game['point_2'];?> </li> <?php } ?>
-        </ul>
-    </main>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-snacks-b1</title>
+    </head>
+    <body>
+        <main>
+            <ul>
+                <?php foreach ($games as $game){ ?>
+                <li> <?php echo $game['team_1']; ?> - <?php echo $game['team_2']; ?> | <?php echo $game['point_1']; ?> - <?php echo $game['point_2'];?> </li> <?php } ?>
+            </ul>
+        </main>
+    </body>
 </html>
 
 <!-- Snack 1
